5715b277942ead053937fb50531f75aecb996a46
BayanALriyati <bayanalriyati@gmail.com>
2024-05-05 22:52:52 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Effect from './hooks/Effect/Effect';
 import State from './hooks/State/State';
 import State1 from './hooks/State/State1';
 
 function App() {
   return (
     <div className="App">
-    <State />
-    <State1 />
+    {/* <State />
+    <State1 /> */}
+    <Effect/>
     </div>
   );
 }
